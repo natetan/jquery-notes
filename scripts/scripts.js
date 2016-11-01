630d8e4ffd680a3e89f2a61b35ca6c6af4d2f0c4
Yulong Tan <swifties@uw.edu>
2016-11-01 01:11:06 -0700

    button color of turn red button is red

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,4 +22,35 @@ $(document).ready(function() {
 		$('#panel2').slideDown(750);
 		$('#panel3').slideDown(750);
 	});
+
+	$('#fadeInButton').on('click', function() {
+		$('#panel1').fadeIn(750);
+		$('#panel2').fadeIn(750);
+		$('#panel3').fadeIn(750);
+	});
+
+	$('#fadeOutButton').on('click', function() {
+		$('#panel1').fadeOut(750);
+		$('#panel2').fadeOut(750);
+		$('#panel3').fadeOut(750);
+	});
+
+	$('#fadeToggleButton').on('click', function() {
+		$('#panel1').fadeToggle(750);
+		$('#panel2').fadeToggle(750);
+		$('#panel3').fadeToggle(750);
+	});
+
+	$('#toggleButton').on('click', function() {
+		$('#panel1').toggle(750);
+		$('#panel2').toggle(750);
+		$('#panel3').toggle(750);
+	});
+
+	$('#turnRedButton').on('click', function() {
+		$('.headers').css({
+			backgroundColor: 'red',
+		});
+	});
+
 });
