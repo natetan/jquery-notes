cc2fe050d8443ab8e00439bef83a3c6a32a05691
Yulong Tan <yulongtan67@gmail.com>
2016-12-11 22:14:16 -0800

    add inital post request function

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,7 +1,16 @@
 $(function() {
 
+	// Fields
 	var $orders = $('#orders');
+	var $name = $('#name');
+	var $drink = $('#drink');
+	var $orderButton = $('#add-order');
 
+	function addOrder(order) {
+		$orders.append('<li class="order-item">name: ' + order.name + ', drink: ' + order.drink + '</li>');
+	}
+
+	// GET 
 	$.ajax({
 		type: 'GET',
 		url: 'api/orders.json',
@@ -9,10 +18,39 @@ $(function() {
 			console.log('success', data);
 
 			// each goes through each item in an array and lets you run a function over them
+			// requires index, and name of each item
 			$.each(JSON.parse(data), function(index, item) {
-				$orders.append('<li class="order-item">name: ' + item.name + ', drink: ' + item.drink + '</li>');
+				addOrder(item);
 			});
+		}, 
+		error: function() {
+			alert('error loading server');
 		}
 	});
+
+	// POST
+	$orderButton.on('click', function() {
+
+		// JavaScript Object
+		var order = {
+			name: $name.val(),
+			drink: $drink.val(), 
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: 'api/orders.json',
+			// must pass in data for post (our order object)
+			data: order,
+			success: function(newItem) {
+				addOrder(newItem);
+			},
+			error: function() {
+				alert('error posting data to server');
+			}
+		});
+
+	});
+
 
 });
