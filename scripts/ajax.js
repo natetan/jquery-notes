268621eece13042bcca70b698e107c1f2107618d
Yulong Tan <yulongtan67@gmail.com>
2016-12-11 21:40:57 -0800

    add initial GET request

---
+++ scripts/ajax.js
@@ -0,0 +1,18 @@
+$(function() {
+
+	var $orders = $('#orders');
+
+	$.ajax({
+		type: 'GET',
+		url: '/api/orders',
+		success: function(data) {
+			console.log('success', data);
+
+			// each goes through each item in an array and lets you run a function over them
+			$.each(data, function(index, item) {
+				$orders.append('<li class="order-item">name: ' + item.name + ', drink: ' + item.drink + '</li>');
+			});
+		}
+	});
+
+});
